5c2995083d8c309b0813aac78d034ff03c5f1bd1
KaymoGithub <115141399+KaymoGithub@users.noreply.github.com>
2025-01-26 17:01:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || '',
+  SESSION_ID: process.env.SESSION_ID || 'XSTRO_67_95_07',
   SUDO: process.env.SUDO || '',
-  BOT_INFO: process.env.BOT_INFO || 'αѕтяσχ11;χѕтяσ м∂',
+  BOT_INFO: process.env.BOT_INFO || 'Kay 95O;utlawz MD',
   WARN_COUNT: process.env.WARN_COUNT || 3,
   TIME_ZONE: process.env.TIME_ZONE || 'Africa/Lagos',
   VERSION: '1.3.3',
